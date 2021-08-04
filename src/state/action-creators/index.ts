57cbebae800e97692ea638ce620dab777a8d7055
Philippe Donnette <philippe.donnette@gmail.com>
2021-08-04 07:17:10 +0300

    28:15

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,29 @@
+import { Action } from './../actions/index';
+import { ActionType } from './../action-types/index';
+import { Dispatch } from "redux";
+
+export const depositMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.Deposit,
+            payload: amount
+        })
+    }
+}
+
+export const withdrawMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.Widthdraw,
+            payload: amount
+        })
+    }
+}
+
+export const bankrupt = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.Bankrupt
+        })
+    }
+}
